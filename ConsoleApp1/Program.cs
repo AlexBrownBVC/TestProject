6b20b699e25887ae803205daf6255f10f764ccfb
Alex Brown <acbrown5@gmail.com>
2018-07-15 12:23:40 -0600

    Test project stuff

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Adder
+    {
+        public static float Add(float a, float b)
+        {
+            return a - b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("waa");
+            //Console.WriteLine(Adder.Add(5, 2));
+        }
+    }
+}
